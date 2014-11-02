5aef3ddddce5adb345e9e35af39b12006da99994
Hiroaki Nakamura <hnakamur@gmail.com>
2014-11-03 00:37:04 +0900

    Update install_ansible.sh to support CentOS 6 as well as Ubuntu

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,7 +1,42 @@
 #!/bin/sh
 
+get_distribution() {
+  if [ -f /etc/lsb-release ]; then
+    awk -F= '$1=="DISTRIB_ID" {print tolower($2)}' /etc/lsb-release
+  elif [ -f /etc/system-release-cpe ]; then
+    awk -F: '{print $3}' /etc/system-release-cpe
+  else
+   echo 'unknown'
+  fi
+}
+
+get_os_family() {
+  case `get_distribution` in
+  redhat|centos|amazon)
+    echo 'redhat'
+    ;;
+  debian|ubuntu)
+    echo 'debian'
+    ;;
+  *)
+    echo 'unknown'
+    ;;
+  esac
+}
+
 if [ ! -f /usr/local/bin/ansible ]; then
-  sudo apt-get install -y curl gcc python-dev
+  case `get_os_family` in
+  debian)
+    sudo apt-get install -y curl gcc python-dev
+    ;;
+  redhat)
+    sudo yum install -y curl gcc python-devel
+    ;;
+  *)
+    echo 'Unsupported OS family' 1>&2
+    exit 1
+    ;;
+  esac
   curl -kL https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python
   sudo pip install ansible
   sudo mkdir -p /etc/ansible
